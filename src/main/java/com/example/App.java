c215a8c000cec6524be1ebd32cb43d11950beedb
Daniil999990 <dannydendi@gmail.com>
2023-06-10 23:05:02 +0300

    m

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        Person person = new Person("Daniil", "Vavshko");
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
